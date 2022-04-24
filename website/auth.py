2eec1fe57297fd7ee1089800ce8c38c999472ddd
Sottii <guilhermesotti.capanema@gmail.com>
2022-04-24 18:07:42 -0300

    24/04/22

--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,12 @@
 #5 coisas relacionadas à autenticação 
-from crypt import methods
+
 from flask import Blueprint, render_template, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods= ['GET', 'POST'])
+@auth.route('/login')
 def login():
-    return render_template('login.html', )
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
