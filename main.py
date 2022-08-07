f662507a1adeb8043f8bbda1ddf7e61b20af2933
Guilherme Sotti <guilhermesotti.capanema@gmail.com>
2022-08-07 19:48:06 -0300

    07/08/22

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':  #3 só se rodar o app isso funciona
-    app.run(debug=True)  #4 sempre que fizermos uma mudança, automaticamente, rerun o servidor
+    app.run(debug=True)  #4 sempre qu1
